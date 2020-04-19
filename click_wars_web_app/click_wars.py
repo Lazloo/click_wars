1037f5c7ef54d53032900307ad5b98e4f7b613e8
Lazloo <lars.freier@posteo.de>
2020-04-19 15:18:32 +0200

    20200419 - Speak with DB
    Signed-off-by: Lazloo <lars.freier@posteo.de>

---
+++ click_wars_web_app/click_wars.py
@@ -0,0 +1,93 @@
+import sqlalchemy
+import MySQLdb
+import sshtunnel
+import get_pw
+import pandas as pd
+
+
+class ClickWars:
+    def __init__(self):
+        self.local_use = True
+        self.db_host = 'lazloo.mysql.pythonanywhere-services.com'
+        self.host_local = '127.0.0.1'
+        self.port_db = 3306
+        self.user, self.pw, self.pw_db = get_pw.get_pw()
+        sshtunnel.SSH_TIMEOUT = 5.0
+        sshtunnel.TUNNEL_TIMEOUT = 5.0
+        pass
+
+    def create_ssh_connection(self):
+        tunnel = sshtunnel.SSHTunnelForwarder(
+            'ssh.pythonanywhere.com',
+            ssh_username=self.user, ssh_password=self.pw,
+            remote_bind_address=(self.db_host, self.port_db)
+        )
+        return tunnel
+
+    def create_mysql_connection(self, host, port, db: str = 'lazloo$click_wars'):
+        connection = MySQLdb.connect(
+            user=self.user, password=self.pw_db,
+            host=host, port=port,
+            database=db)
+
+        return connection
+
+    def create_sqlalchemy_connection(self, host, port, db: str = 'lazloo$click_wars'):
+        conn_address = 'mysql://' + self.user + ':' + self.pw_db + '@' + host + ':' + str(port) + '/' + db
+        engine = sqlalchemy.create_engine(conn_address, pool_recycle=280)
+
+        return engine
+
+    def return_dataframe(self, sql: str):
+        if self.local_use:
+            with self.create_ssh_connection() as tunnel:
+                con = self.create_mysql_connection(host=self.host_local, port=tunnel.local_bind_port)
+        else:
+            con = self.create_mysql_connection(host=self.db_host, port=self.port_db)
+        df = pd.read_sql(sql=sql, con=con)
+        con.close()
+        return df
+
+    def commit_sql_query(self, sql: str):
+        if self.local_use:
+            with self.create_ssh_connection() as tunnel:
+                con = self.create_mysql_connection(host=self.host_local, port=tunnel.local_bind_port)
+        else:
+            con = self.create_mysql_connection(host=self.db_host, port=self.port_db)
+
+        my_cursor = con.cursor()
+        my_cursor.execute(sql)
+        con.commit()
+        # my_cursor.commit()
+        my_cursor.close()
+        con.close()
+
+    def update_click(self, session_id: int, player_id):
+        sql_update = """
+        UPDATE lazloo$click_wars.click_distribution
+        SET 
+            count_clicks = count_clicks + 1
+        where session_id = /*session_id*/ and player_id = /*player_id*/
+        ;
+        """.replace('/*session_id*/', str(session_id)).replace('/*player_id*/', str(player_id))
+        self.commit_sql_query(sql=sql_update)
+
+    def reset_clicks(self, session_id: int):
+        sql_update = """
+        UPDATE lazloo$click_wars.click_distribution
+        SET 
+            count_clicks = 1
+        where session_id = /*session_id*/ 
+        ;
+        """.replace('/*session_id*/', str(session_id))
+        self.commit_sql_query(sql=sql_update)
+
+    def get_clicks(self, session_id):
+
+        sql = """
+            select * 
+            from lazloo$click_wars.click_distribution
+            where session_id = /*session_id*/
+        """.replace('/*session_id*/', str(session_id))
+        df = self.return_dataframe(sql=sql)
+        return df
--- run_click_wars.py
+++ run_click_wars.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pandas.io.json import json_normalize
+from click_wars_web_app import click_wars
+from flask_cors import CORS
 
+obj_click = click_wars.ClickWars()
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def home():
+    res = render_template('click_wars.html')
+    return res
+
+
+@app.route('/get_clicks')
+def get_clicks():
+    df_clicks = obj_click.get_clicks(session_id=1)
+    # request.form.
+    # return render_template('click_wars.html')
+    return df_clicks.to_json(orient="columns")
+    # return df_clicks.to_json(orient="values")
+    # return json_normalize(df_clicks.to_dict())
+
+
+@app.route('/update_progress_bar')
+def update_progress_bar():
+    result_bar = request.form.getlist('result_bar')
+    df_clicks = obj_click.get_clicks(session_id=1)
+    return render_template('click_wars.html')
+
+
+# /<int: player_id>/<int: session_id>
+@app.route('/update_click', methods=['POST', 'GET'])
+def update_click():
+    print('update')
+    player_id = request.args.get('player_id')
+    session_id = request.args.get('session_id')
+    obj_click.update_click(player_id=player_id, session_id=session_id)
     return render_template('click_wars.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=10004)
